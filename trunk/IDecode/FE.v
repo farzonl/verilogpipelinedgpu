1ab3c185b45cda790d5e2c0b578ab667464e75c6
BavarianS6@gmail.com <BavarianS6@gmail.com@c000d2b2-ac4c-2ab2-e9a6-1164e6397ead>
2012-04-06 17:17:44 +0000

    added stall

--- trunk/IDecode/FE.v
+++ trunk/IDecode/FE.v
@@ -1,5 +1,8 @@
-module FE(CLOCK_50,reset,id_instr,Loop,PC_in,PC_out);
-input CLOCK_50,reset,Loop;
+
+
+
+module FE(CLOCK_50,reset,Stall,id_instr,Loop,PC_in,PC_out);
+input CLOCK_50,reset,Loop,Stall;
 input[15:0] PC_in;
 output reg [31:0] id_instr;
 output reg [15:0] PC_out;
@@ -20,20 +23,27 @@ always @(posedge CLOCK_50 or posedge reset) begin
 		id_instr <= 0;
 		PC_out <= 0;
 	end
+	else if(~Stall) begin
+	
+		if(Loop) pc <= PC_in;
+		else pc <= pc + 16'd1;
+		
+		if(Loop) mdr <= mem[PC_in];
+		else mdr <= mem[pc];
+		
+		id_instr <= mdr;
+		
+		if(Loop) PC_out <= PC_in;
+		else PC_out <= pc;
+	
+	end
 	else begin
-	
-	if(Loop) pc <= PC_in;
-	else pc <= pc + 16'd1;
-	
-	if(Loop) mdr <= mem[PC_in];
-	else mdr <= mem[pc];
-	
-	id_instr <= mdr;
-	
-	if(Loop) PC_out <= PC_in;
-	else PC_out <= pc;
-	
+		pc<=pc;
+		mdr<=mdr;
+		id_instr<=id_instr;
+		PC_out<=pc;
 	end
 end
 
 endmodule
+
