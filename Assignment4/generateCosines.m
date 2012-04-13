c94f016e537e3984cdc1a594a61e676e37faae83
brett.dutro@gmail.com <brett.dutro@gmail.com@c000d2b2-ac4c-2ab2-e9a6-1164e6397ead>
2012-04-13 01:52:09 +0000

    Fixed for [-255,255] range, 2's complement

--- Assignment4/generateCosines.m
+++ Assignment4/generateCosines.m
@@ -17,15 +17,17 @@
 function generateCosines ()
 
 fid = fopen("cosine_LUT.v","w");
-fprintf(fid,"//Returns cos(INVAL) where INVAL is in the range [-360,360] in 1.8.7 format\n\n");
-fprintf(fid,"module cosine_LUT(INVAL,OUTVAL);\ninput[15:0] INVAL;\noutput[15:0] OUTVAL;\n\n//Round INVAL to the nearest degree\nwire[15:0] INVAL_rounded = {INVAL[15:8],(INVAL[6] == 1'b1 || INVAL[7] == 1'b1) ? 1'b1 : 1'b0,7'b0};\n\nassign OUTVAL = ");
+fprintf(fid,"//Returns cos(INVAL) where INVAL is in the range [-255,255] in 1.8.7 format\n\n");
+fprintf(fid,"module cosine_LUT(INVAL,OUTVAL);\ninput[15:0] INVAL;\noutput[15:0] OUTVAL;\n\n//Round INVAL to the nearest degree\nwire[15:0] INVAL_rounded = (INVAL[15] == 1 && INVAL[7] == 1'b1) ? {INVAL[15:7] - 1'b1,7'b0} : (INVAL[15] == 0 && INVAL[7] == 1'b1) ? {INVAL[15:7] + 1'b1,7'b0} : {INVAL[15:7],7'b0};\n\nassign OUTVAL = ");
 for x = 0:180
     res = cos(x*pi/180);
     res_fixed = doubleToFixed(res);
     if(x == 180)
         fprintf(fid,"(INVAL_rounded == 16'h%x || INVAL_rounded == 16'h%x) ? 16'h%x : 16'hXXXX;\n",doubleToFixed(x), doubleToFixed(-1*x), res_fixed);
+    elseif(360 - x <= 255)
+        fprintf(fid,"(INVAL_rounded == 16'h%x || INVAL_rounded == 16'h%x || INVAL_rounded == 16'h%x || INVAL_rounded == 16'h%x) ? 16'h%x : \n\t\t\t\t",doubleToFixed(x),doubleToFixed(-1*x),doubleToFixed(360 - x),doubleToFixed(-360 + x),res_fixed);
     else
-        fprintf(fid,"(INVAL_rounded == 16'h%x || INVAL_rounded == 16'h%x || INVAL_rounded == 16'h%x || INVAL_rounded == 16'h%x) ? 16'h%x : \n\t\t\t\t",doubleToFixed(x),doubleToFixed(360-x),doubleToFixed(-1*x),doubleToFixed(-360+x),res_fixed);
+        fprintf(fid,"(INVAL_rounded == 16'h%x || INVAL_rounded == 16'h%x) ? 16'h%x : \n\t\t\t\t",doubleToFixed(x),doubleToFixed(-1*x),res_fixed);
     end
 end
 fprintf(fid,"endmodule\n");
