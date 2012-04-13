6a8506c01dc862c69546eadd5529e903852f4567
brett.dutro@gmail.com <brett.dutro@gmail.com@c000d2b2-ac4c-2ab2-e9a6-1164e6397ead>
2012-04-13 01:52:41 +0000

    Fixed for [-255,255] range, 2's complement

--- trunk/Assignment4/generateSines.m
+++ trunk/Assignment4/generateSines.m
@@ -17,26 +17,23 @@
 function generateSines ()
 
 fid = fopen("sine_LUT.v","w");
-fprintf(fid,"//Returns sin(INVAL) where INVAL is in the range [-360,360] in 1.8.7 format\n\n");
-fprintf(fid,"module sine_LUT(INVAL,OUTVAL);\ninput[15:0] INVAL;\noutput[15:0] OUTVAL;\n\n//Round INVAL to the nearest degree\nwire[15:0] INVAL_rounded = {INVAL[15:8],(INVAL[6] == 1'b1 || INVAL[7] == 1'b1) ? 1'b1 : 1'b0,7'b0};\n\nassign OUTVAL = ");
-for x = 0:90
+fprintf(fid,"//Returns sin(INVAL) where INVAL is in the range [-255,255] in 1.8.7 format\n\n");
+fprintf(fid,"module sine_LUT(INVAL,OUTVAL);\ninput[15:0] INVAL;\noutput[15:0] OUTVAL;\n\n//Round INVAL to the nearest degree\nwire[15:0] INVAL_rounded = (INVAL[15] == 1 && INVAL[7] == 1'b1) ? {INVAL[15:7] - 1'b1,7'b0} : (INVAL[15] == 0 && INVAL[7] == 1'b1) ? {INVAL[15:7] + 1'b1,7'b0} : {INVAL[15:7],7'b0};\n\nassign OUTVAL = ");
+for x = -90:90
     res = sin(x*pi/180);
     res_fixed = doubleToFixed(res);
     if(x == 90)
-        fprintf(fid,"(INVAL_rounded == 16'h%x || INVAL_rounded == 16'h%x) ? 16'h%x : \n\t\t\t\t",doubleToFixed(x),doubleToFixed(-360+x), res_fixed);
-    elseif(x == 0)
-        fprintf(fid,"(INVAL_rounded == 16'h%x || INVAL_rounded == 16'h%x || INVAL_rounded == 16'h%x) ? 16'h%x : \n\t\t\t\t",doubleToFixed(x),doubleToFixed(180-x),doubleToFixed(360-x),res_fixed);
-    else
-        fprintf(fid,"(INVAL_rounded == 16'h%x || INVAL_rounded == 16'h%x || INVAL_rounded == 16'h%x || INVAL_rounded == 16'h%x) ? 16'h%x : \n\t\t\t\t",doubleToFixed(x),doubleToFixed(180-x),doubleToFixed(-360+x),doubleToFixed(-360+180-x),res_fixed);
-    end
-end
-for x = 181:270
-    res = sin(x*pi/180);
-    res_fixed = doubleToFixed(res);
-    if(x == 270)
-        fprintf(fid,"(INVAL_rounded == 16'h%x || INVAL_rounded == 16'h%x) ? 16'h%x : 16'hXXXX;\n",doubleToFixed(x),doubleToFixed(-360+x), res_fixed);
-    else
-        fprintf(fid,"(INVAL_rounded == 16'h%x || INVAL_rounded == 16'h%x || INVAL_rounded == 16'h%x || INVAL_rounded == 16'h%x) ? 16'h%x : \n\t\t\t\t",doubleToFixed(x),doubleToFixed(x+90),doubleToFixed(-360+x),doubleToFixed(-360+x+90),res_fixed);
+        fprintf(fid,"(INVAL_rounded == 16'h%x) ? 16'h%x : 16'hXXXX;\n",doubleToFixed(x), res_fixed);
+    elseif(x == -90)
+        fprintf(fid,"(INVAL_rounded == 16'h%x) ? 16'h%x : \n\t\t\t\t",doubleToFixed(x), res_fixed);
+    elseif(x < 90 && x >= 0 && -360 + x >= -255)
+        fprintf(fid,"(INVAL_rounded == 16'h%x || INVAL_rounded == 16'h%x || INVAL_rounded == 16'h%x) ? 16'h%x : \n\t\t\t\t",doubleToFixed(x),doubleToFixed(180-x),doubleToFixed(-360 + x),res_fixed);
+    elseif(x < 0 && x > -90 && 360 + x <= 255)
+        fprintf(fid,"(INVAL_rounded == 16'h%x || INVAL_rounded == 16'h%x || INVAL_rounded == 16'h%x) ? 16'h%x : \n\t\t\t\t",doubleToFixed(x),doubleToFixed(-180-x),doubleToFixed(360 + x),res_fixed);
+    elseif(x < 90 && x >= 0)
+        fprintf(fid,"(INVAL_rounded == 16'h%x || INVAL_rounded == 16'h%x) ? 16'h%x : \n\t\t\t\t",doubleToFixed(x),doubleToFixed(180-x),res_fixed);
+    elseif(x < 0 && x > -90)
+        fprintf(fid,"(INVAL_rounded == 16'h%x || INVAL_rounded == 16'h%x) ? 16'h%x : \n\t\t\t\t",doubleToFixed(x),doubleToFixed(-180-x),res_fixed);
     end
 end
 fprintf(fid,"endmodule\n");
