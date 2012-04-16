4971a72687c66f12bfd732dc9a7b0236037793f1
jpikeGames@gmail.com <jpikeGames@gmail.com@c000d2b2-ac4c-2ab2-e9a6-1164e6397ead>
2012-04-16 21:36:22 +0000

    Added some demo program code

---
+++ trunk/demo_code/test_asm_assign5.s
@@ -0,0 +1,104 @@
+movi.f r8 0.0f
+movi.f r9 0.0f
+movi.f r10 0.0f
+loopcount 10	
+startloop
+pushmatrix
+loadidentity
+vcompmovi v1 1 -1.5f
+vcompmovi v1 2 0.0f
+translate v1
+vcompmov v0 0 r8
+vcompmovi v0 1 0.0f
+vcompmovi v0 2 0.0f
+vcompmovi v0 3 1.0f
+rotate v0
+startprimitive 4
+vcompmovi v2 0 255
+vcompmovi v2 1 255
+vcompmovi v2 2 255
+color v2
+vcompmovi v3 1 -1.0f
+vcompmovi v3 2 1.0f
+vcompmovi v3 3 1.0f
+setvertex v3
+vcompmovi v3 1 1.0f
+vcompmovi v3 2 1.0f
+vcompmovi v3 3 1.0f
+setvertex v3
+vcompmovi v3 1 0.0f
+vcompmovi v3 2 -1.0f
+vcompmovi v3 3 1.0f
+setvertex v3		
+endprimitive
+addi.f r8 r8 20.0f	
+popmatrix	
+
+pushmatrix
+loadidentity
+vcompmovi v4 1 0.0f
+vcompmovi v4 2 0.0f
+translate v4
+vcompmov v5 0 r9
+vcompmovi v5 1 0.0f
+vcompmovi v5 2 0.0f
+vcompmovi v5 3 -1.0f
+rotate v5
+startprimitive 4
+vcompmovi v6 0 255
+vcompmovi v6 1 0
+vcompmovi v6 2 0
+color v6
+vcompmovi v7 1 1.0f
+vcompmovi v7 2 -1.0f
+vcompmovi v7 3 0.0f
+setvertex v7
+vcompmovi v7 1 -1.0f
+vcompmovi v7 2 -1.0f
+vcompmovi v7 3 0.0f
+setvertex v7
+vcompmovi v7 1 0.0f
+vcompmovi v7 2 1.0f
+vcompmovi v7 3 0.0f
+setvertex v7		
+endprimitive
+addi.f r9 r9 20.0f	
+popmatrix	
+
+pushmatrix
+loadidentity
+vcompmovi v8 1 0.0f
+vcompmovi v8 2 1.0f
+translate v8
+vcompmov v9 0 r9
+vcompmovi v9 1 0.0f
+vcompmovi v9 2 0.0f
+vcompmovi v9 3 -1.0f
+rotate v9
+startprimitive 7
+vcompmovi v10 0 0
+vcompmovi v10 1 255
+vcompmovi v10 2 0
+color v10
+vcompmovi v11 1 0.0f
+vcompmovi v11 2 0.0f
+vcompmovi v11 3 -1.0f
+setvertex v11
+vcompmovi v11 1 0.0f
+vcompmovi v11 2 1.0f
+vcompmovi v11 3 -1.0f
+setvertex v11
+vcompmovi v11 1 1.0f
+vcompmovi v11 2 1.0f
+vcompmovi v11 3 -1.0f
+setvertex v11		
+vcompmovi v11 1 1.0f
+vcompmovi v11 2 0.0f
+vcompmovi v11 3 -1.0f
+setvertex v11	
+endprimitive
+addi.f r10 r10 20.0f	
+popmatrix	
+
+draw
+endloop
