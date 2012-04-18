a4edbf7fcdb35b16685dc1d7d4042b5974c691d6
BavarianS6@gmail.com <BavarianS6@gmail.com@c000d2b2-ac4c-2ab2-e9a6-1164e6397ead>
2012-04-18 18:04:38 +0000

    Now draws 2 triangles. Rectangle breaks.

--- trunk/Assignment_5/gpu_testbench.v
+++ trunk/Assignment_5/gpu_testbench.v
@@ -7,14 +7,15 @@ module gpu_testbench(iGPU_DATA, oGPU_DATA, oMEM_ADDR, oMEM_READ, oMEM_WRITE,
 input 		 [15:0]	iGPU_DATA;
 output	reg [15:0]	oGPU_DATA;
 output	reg [17:0]	oMEM_ADDR;
+output   reg [15:0]  oFPS;
 output	reg 			oMEM_WRITE;
 output	reg			oMEM_READ;
-output   reg [15:0]  oFPS;
 
 input	iVIDEO_ON;
 input	iCLK;
 input iRST_N;
 input iVGA_BLANK;
+//assign reset = iRST_N;
 
 	wire reset, draw, StartPrim, EndPrim, loop, stall;
 	reg NewVertex_delayed;//, NewVertex;
@@ -29,7 +30,6 @@ input iVGA_BLANK;
 	wire [63:0] vert0Vector;
 	
 	reg rastCycle1, rastCycle2, rastCycle3, rastCycle4, rastContinue;
-	wire rast1, rast2, rast3, rast4, rastCont;
 	wire [1:0] v0depth, v1depth, v2depth, pixelDepth;
 	wire [15:0] triangleColor, loop_PC, PC;
 	wire writePixel, rastDone;
@@ -42,21 +42,21 @@ input iVGA_BLANK;
 	reg [31:0] primitive1_v0_buffer;
 	reg [31:0] primitive1_v1_buffer;
 	reg [31:0] primitive1_v2_buffer;
-   reg [31:0] primitive1_v3_buffer;
-	reg [31:0] primitive1_min;
+  reg [31:0] primitive1_v3_buffer;
+  	reg [31:0] primitive1_min;
    reg [31:0] primitive1_max;
-   reg [3:0] primitive1_type;
-   reg primitive1_valid;
+  reg [3:0] primitive1_type;
+  reg primitive1_valid;
  	reg primitive1_drawn;
+	reg resetBlank;
 	reg blanked1;
-	
   
  	reg [31:0] primitive2_v0_buffer;
 	reg [31:0] primitive2_v1_buffer;
 	reg [31:0] primitive2_v2_buffer;
   reg [31:0] primitive2_v3_buffer;
-  reg [31:0] primitive2_min;
-  reg [31:0] primitive2_max;
+  	reg [31:0] primitive2_min;
+   reg [31:0] primitive2_max;
   reg [3:0] primitive2_type;
   reg primitive2_valid;
  	reg primitive2_drawn;
@@ -80,12 +80,8 @@ input iVGA_BLANK;
 	reg [31:0] counter;
 	reg [2:0] vertexCount;
 	reg [17:0] i;
-	reg [15:0] draw_counter;
-	reg [1:0] VS_count;
-	reg clock;
 	
-	reg latch, primRead, blankDone;
-	wire V_sync_slow;
+	reg latch, primRead, blankDone, prim_count;
 
 
 	assign triangleColor = 16'h0FFF;
@@ -93,6 +89,10 @@ input iVGA_BLANK;
 	assign v1depth = 0;
 	assign v2depth = 0;
 	assign reset = !iRST_N;
+	
+	wire rast1, rast2, rast3, rast4, rastCont;
+	wire [63:0] id_to_vops_vector;
+	wire [3:0] vertexOp;
 	
 	/*assign rast1 = iVGA_BLANK ? rastCycle1 : 1'b0;
 	assign rast2 = iVGA_BLANK ? rastCycle2 : 1'b0;
@@ -129,8 +129,8 @@ input iVGA_BLANK;
   initial rowInd = 0;
   initial colInd = 0;
   initial blankDone = 1;
-  initial VS_count = 0;
-  initial draw_counter = 16'd0;
+  initial prim_count = 1'b0;
+  initial current_prim = 1'b0;
   
 	
 	initial vertexCount = 3'b000;
@@ -144,7 +144,7 @@ input iVGA_BLANK;
 				.PC_out(PC));
 				
 				
-	ID decode(.RESET(reset),.CLK(iCLK),
+	/*ID decode(.RESET(reset),.CLK(iCLK),
 				 .Stall(stall),
 				 .Instruction(instruction),
 				 .Vertex(vert0Vector),
@@ -153,7 +153,20 @@ input iVGA_BLANK;
 				 .PrimitiveType(PrimType),
 				 .Draw(draw),.Loop(loop),
 				 .PC(PC),.PC_Out(loop_PC),
-				 .NewVertex(NewVertex));
+				 .NewVertex(NewVertex));*/
+				 
+	ID decode(.RESET(reset),.CLK(iCLK),
+				 .Stall(stall),
+				 .Instruction(instruction),
+				 .Vertex(id_to_vops_vector),
+				 .StartPrimitive(StartPrim),
+				 .EndPrimitive(EndPrim),
+				 .PrimitiveType(PrimType),
+				 .Draw(draw),.Loop(loop),
+				 .PC(PC),.PC_Out(loop_PC),
+				 .VertexOp(vertexOp));
+				 
+	vertexops vertops(.CLK(iCLK), .vectorIn(id_to_vops_vector), .Vertex(vert0Vector), .NewVertex(NewVertex),.op(vertexOp),.stall(stall));
 				 
 				 	//Single cycle unit					
 	VectorComponentExtractor vect0Extract(.in_vector_val(vert0Vector), .clock(iCLK),
@@ -170,16 +183,11 @@ input iVGA_BLANK;
 	initial counter = 32'd0;
 	initial NewVertex_delayed = 1'b0;
 	initial draw_reg = 1'b0;
+	initial blanked1 = 1'b0;
+	initial primitive2_max = 32'd0;
+	initial primitive2_min = 32'd0;
 	
-	assign stall = /*!iVGA_BLANK |*/ !blankDone |  rastCycle1 | rastCycle2 | rastCycle3 | rastCycle4 | rastContinue;
-	
-	assign V_sync_slow = (VS_count==2'b11) ? 1'b1 : 1'b0;
-	
-	/*always @ (posedge iCLK) begin
-		//counter for V-sync
-		//gives a V-sync that goes high every 4th v-sync
-		clock <= clock + 1'b1;
-	end*/
+	assign stall = /*!iVGA_BLANK |*/ !blankDone | rastCycle1 | rastCycle2 | rastCycle3 | rastCycle4 | rastContinue;
 	
 				 
 	always @ (posedge iCLK or posedge reset) begin
@@ -194,167 +202,163 @@ input iVGA_BLANK;
 		oGPU_DATA <= {4'h0, 4'hF, 4'h0, 4'h0};		
 	end else begin
 	
+		if (!iVIDEO_ON /*&& iVGA_BLANK*/) begin 
+	
 		//Set drawn and valid flags to 0. This will be reset later in the code if needed.
-		if (!iVIDEO_ON) begin 
-
-		//if (iVGA_BLANK) begin
-
-		if(StartPrim==1'b1) begin 
-		  primitive2_drawn <= 1'b0;
-		  primitive2_drawn_half <= 1'b0;
-		  primitive1_drawn <= 1'b0;
-		  primitive1_valid <= 1'b0;
-		  primitive2_valid <= 1'b0;
-		  current_prim <= current_prim + 1'b1;
-		  if(current_prim==1'b0) primitive1_type <= PrimType;
+		if(rastDone==1'b1) begin 
+		  if(current_prim==1'b1) begin
+			  primitive2_drawn <= 1'b0;
+			  primitive2_drawn_half <= 1'b0;
+			  primitive2_valid <= 1'b0;
+		  end else begin
+			  primitive1_drawn <= 1'b0;
+			  primitive1_valid <= 1'b0;
+		  end
+		end
+		
+		if(StartPrim) begin
+		  prim_count <= prim_count + 1'b1;
+		  if(prim_count==1'b0) primitive1_type <= PrimType;
 		  else primitive2_type <= PrimType;
 		end
-	  
-	  primType_delayed <= PrimType;
-	  
-	  
-		//Only do anything during vblank. Otherwise, screen will tear.
+
+		if(blankDone==1'b0) begin
+			oMEM_ADDR <= rowInd*18'd640 + colInd;
+			oMEM_WRITE <= 1'b1;
+			oMEM_READ <= 1'b0;
+			oGPU_DATA <= {4'h0, 4'h0, 4'h0, 4'h0};
+			//if(draw==1'b1) draw_reg <= 1'b1;
+		end else if(blankDone==1'b1) begin
+			oMEM_ADDR <= pixelYout*18'd640 + pixelXout;
+			oMEM_WRITE <= writePixel;
+			oGPU_DATA <= pixelColor;//16'hFFFF;
+			oMEM_READ <= 1'b0;
+		end
 		
-		draw_counter <= draw_counter + 1'b1;
-		//if(iVGA_BLANK) begin
-			if(blankDone==1'b0) begin
-				oMEM_ADDR <= rowInd*640 + colInd;
-				oMEM_WRITE <= 1'b1;
-				oMEM_READ <= 1'b0;
-				oGPU_DATA <= {4'hC, 4'h0, 4'h0, 4'h0};
-				//if(draw==1'b1) draw_reg <= 1'b1;
-			end
-			else begin
-				oMEM_ADDR <= pixelYout*18'd640 + pixelXout;
-				oMEM_WRITE <= writePixel;
-				oGPU_DATA <= pixelColor;//16'hFFFF;
-				oMEM_READ <= 1'b0;
-			end
-		//end
+		if(resetBlank==1'b1) resetBlank <= 1'b0;
 	
-		if(!blanked1) begin
-			if(rowInd==primitive1_max[31:16] && colInd==primitive1_max[15:0]) blanked1 <=1'b1;
-			else blanked1 <= 1'b0;
-		end else
-		if(rowInd==primitive2_max[31:16] && colInd==primitive2_max[15:0]) blankDone <=1'b1;
-		else blankDone <= 1'b0;
-		
-		
 		if(draw==1'b1) begin 
-			//oFPS <= 32'd25200000/draw_counter;
 			draw_reg <=1'b1;
-			blankDone <= 1'b0;
-			//reset draw counter;
-			draw_counter <= 16'd0;
-		end else if (((primitive2_drawn & primitive2_valid) | !primitive2_valid) &
-					 ((primitive1_drawn & primitive1_valid) | !primitive1_valid)) draw_reg<=0;
-					 
-					 
+			//blankDone <= 1'b0;
+			resetBlank <= 1'b1;
+			blanked1 <= 1'b0;
+		end
+		else if ( ((primitive2_drawn & primitive2_valid) | !primitive2_valid) &
+					 ((primitive1_drawn & primitive1_valid) | !primitive1_valid) )  draw_reg<=1'b0;
 		counter <= counter+1;
 		
 		//delay signal for 1 cycle to allow Converter to do its thing
 		NewVertex_delayed <= NewVertex;
+	
 		
-		//delay draw for 2 cycles on the off chance that the last instruction set a vertex.
-		//draw_delayed2 <= draw_delayed1;
-		//draw_delayed1 <= draw;
+		if(!blanked1) begin
+			if(rowInd==primitive1_max[15:0] && colInd==primitive1_max[31:16]) begin
+					blanked1 <=1'b1;
+					//blankDone <=1'b1;
+			end
+			else blanked1 <= 1'b0;
+		end else
+		if((rowInd==primitive2_max[15:0] && colInd==primitive2_max[31:16]) || primitive2_valid==1'b0) blankDone <=1'b1;
+		//else// blankDone <= 1'b0;
 		
+		//oFPS <= {blanked1, 5'd0, rowInd};
+		oFPS <= {primitive1_type, primitive2_type};
+			
+			
 		if(NewVertex_delayed) begin
 		  
 		  if(primitive1_valid==0) begin
-		
-				if(vertexCount==0) begin
-					primitive1_min <= {v0x, v0y};
-					primitive1_max <= {v0x, v0y};
-					primitive1_v0_buffer <= {v0x, v0y};
-					vertexCount <= vertexCount + 1'b1;
-				end else
-				if(vertexCount==1) begin
-					if(v0x<primitive1_min[31:16]) primitive1_min[31:16] <= v0x;
-					else if(v0x>primitive1_max[31:16]) primitive1_max[31:16] <= v0x;
-					if(v0y<primitive1_min[15:0]) primitive1_min[15:0] <= v0y;
-					else if(v0y>primitive1_max[15:0]) primitive1_max[15:0] <= v0y;
-					
-					primitive1_v1_buffer <= {v0x, v0y};
-					vertexCount <= vertexCount + 1'b1;
-				end else
-				if(vertexCount==2) begin
-					if(v0x<primitive1_min[31:16]) primitive1_min[31:16] <= v0x;
-					else if(v0x>primitive1_max[31:16]) primitive1_max[31:16] <= v0x;
-					if(v0y<primitive1_min[15:0]) primitive1_min[15:0] <= v0y;
-					else if(v0y>primitive1_max[15:0]) primitive1_max[15:0] <= v0y;
-					
-					primitive1_v2_buffer[31:16]<=v0x;
-					primitive1_v2_buffer[15:0]<=v0y;
-					vertexCount <= vertexCount + 1'b1;
-					primitive1_valid <= 1'b1;
-				end else if(vertexCount==3)begin 
-					if(v0x<primitive1_min[31:16]) primitive1_min[31:16] <= v0x;
-					else if(v0x>primitive1_max[31:16]) primitive1_max[31:16] <= v0x;
-					if(v0y<primitive1_min[15:0]) primitive1_min[15:0] <= v0y;
-					else if(v0y>primitive1_max[15:0]) primitive1_max[15:0] <= v0y;
-					
-				  if(primitive1_type == `RECTANGLE) begin
-					  primitive1_v3_buffer[31:16]<=v0x;
-					  primitive1_v3_buffer[15:0]<=v0y;
-				  end
-				  vertexCount <= 3'd0;
-				  primitive1_valid <= 1'b1;
-				end
-  			
-			end else if (primitive2_valid==0) begin
+			
+					if(vertexCount==0) begin
+						primitive1_min <= {v0x, v0y};
+						primitive1_max <= {v0x, v0y};
+						primitive1_v1_buffer <= {v0x,v0y};
+						vertexCount <= vertexCount + 1'b1;
+					end else
+					if(vertexCount==1) begin
+						if(v0x<primitive1_min[31:16]) primitive1_min[31:16] <= v0x;
+						else if(v0x>primitive1_max[31:16]) primitive1_max[31:16] <= v0x;
+						if(v0y<primitive1_min[15:0]) primitive1_min[15:0] <= v0y;
+						else if(v0y>primitive1_max[15:0]) primitive1_max[15:0] <= v0y;
+						
+						primitive1_v2_buffer[31:16] <= v0x;
+						primitive1_v2_buffer[15:0] <= v0y;
+						vertexCount <= vertexCount + 1'b1;
+					end else
+					if(vertexCount==2) begin
+						if(v0x<primitive1_min[31:16]) primitive1_min[31:16] <= v0x;
+						else if(v0x>primitive1_max[31:16]) primitive1_max[31:16] <= v0x;
+						if(v0y<primitive1_min[15:0]) primitive1_min[15:0] <= v0y;
+						else if(v0y>primitive1_max[15:0]) primitive1_max[15:0] <= v0y;
+						
+						primitive1_v0_buffer[31:16]<=v0x;
+						primitive1_v0_buffer[15:0]<=v0y;
+						if(primitive1_type!=`RECTANGLE) begin
+							vertexCount <= 3'd0;
+							primitive1_valid <= 1'b1;
+						end else vertexCount <= vertexCount + 1'b1;
+					end else if(vertexCount==3)begin						
+					  if(primitive1_type == `RECTANGLE) begin
+						  if(v0x<primitive1_min[31:16]) primitive1_min[31:16] <= v0x;
+						  else if(v0x>primitive1_max[31:16]) primitive1_max[31:16] <= v0x;
+						  if(v0y<primitive1_min[15:0]) primitive1_min[15:0] <= v0y;
+						  else if(v0y>primitive1_max[15:0]) primitive1_max[15:0] <= v0y;
+						  primitive1_v3_buffer[31:16]<=v0x;
+						  primitive1_v3_buffer[15:0]<=v0y;
+						  vertexCount <= 3'd0;
+						  primitive1_valid <= 1'b1;
+					  end
+					end
+				
+				end else if (primitive2_valid==0) begin
 
-			  
-				if(vertexCount==0) begin
-					primitive2_min <= {v0x, v0y};
-					primitive2_max <= {v0x, v0y};					
-					primitive2_v1_buffer[31:16]<=v0x;
-					primitive2_v1_buffer[15:0]<=v0y;
-					vertexCount <= vertexCount + 1'b1;
+				  
+					if(vertexCount==0) begin
+						primitive2_min <= {v0x, v0y};
+						primitive2_max <= {v0x, v0y};					
+						primitive2_v1_buffer[31:16]<=v0x;
+						primitive2_v1_buffer[15:0]<=v0y;
+						vertexCount <= vertexCount + 1'b1;
+					end else
+					if(vertexCount==1) begin
+						if(v0x<primitive2_min[31:16]) primitive2_min[31:16] <= v0x;
+						else if(v0x>primitive2_max[31:16]) primitive2_max[31:16] <= v0x;
+						if(v0y<primitive2_min[15:0]) primitive2_min[15:0] <= v0y;
+						else if(v0y>primitive2_max[15:0]) primitive2_max[15:0] <= v0y;
+						
+						primitive2_v2_buffer[31:16]<=v0x;
+						primitive2_v2_buffer[15:0]<=v0y;
+						vertexCount <= vertexCount + 1'b1;
+					end else
+					if(vertexCount==2) begin
+						if(v0x<primitive2_min[31:16]) primitive2_min[31:16] <= v0x;
+						else if(v0x>primitive2_max[31:16]) primitive2_max[31:16] <= v0x;
+						if(v0y<primitive2_min[15:0]) primitive2_min[15:0] <= v0y;
+						else if(v0y>primitive2_max[15:0]) primitive2_max[15:0] <= v0y;
+						
+						primitive2_v0_buffer[31:16]<=v0x;
+						primitive2_v0_buffer[15:0]<=v0y;
+						if(primitive2_type!=`RECTANGLE) begin
+						  vertexCount <= 3'b0;
+						  primitive2_valid <= 1'b1;
+						end else vertexCount <= vertexCount + 1'b1;
+					end else
+					if(vertexCount==3)begin 						
+					  if(primitive2_type == `RECTANGLE) begin
+					  		if(v0x<primitive2_min[31:16]) primitive2_min[31:16] <= v0x;
+							else if(v0x>primitive2_max[31:16]) primitive2_max[31:16] <= v0x;
+							if(v0y<primitive2_min[15:0]) primitive2_min[15:0] <= v0y;
+							else if(v0y>primitive2_max[15:0]) primitive2_max[15:0] <= v0y;
+						  primitive2_v3_buffer[31:16]<=v0x;
+						  primitive2_v3_buffer[15:0]<=v0y;
+					  end
+					  vertexCount <= 3'd0;
+					  primitive2_valid <= 1'b1;
+					end
 				end
-				if(vertexCount==1) begin
-					if(v0x<primitive2_min[31:16]) primitive2_min[31:16] <= v0x;
-					else if(v0x>primitive2_max[31:16]) primitive2_max[31:16] <= v0x;
-					if(v0y<primitive2_min[15:0]) primitive2_min[15:0] <= v0y;
-					else if(v0y>primitive1_max[15:0]) primitive2_max[15:0] <= v0y;
-					
-					primitive2_v2_buffer[31:16]<=v0x;
-					primitive2_v2_buffer[15:0]<=v0y;
-					vertexCount <= vertexCount + 1'b1;
-				end
-				if(vertexCount==2) begin
-					if(v0x<primitive2_min[31:16]) primitive2_min[31:16] <= v0x;
-					else if(v0x>primitive2_max[31:16]) primitive2_max[31:16] <= v0x;
-					if(v0y<primitive2_min[15:0]) primitive2_min[15:0] <= v0y;
-					else if(v0y>primitive1_max[15:0]) primitive2_max[15:0] <= v0y;
-					
-					primitive2_v0_buffer[31:16]<=v0x;
-					primitive2_v0_buffer[15:0]<=v0y;
-					vertexCount <= vertexCount + 1'b1;
-				end
-				if(vertexCount==3)begin 
-					if(v0x<primitive2_min[31:16]) primitive2_min[31:16] <= v0x;
-					else if(v0x>primitive2_max[31:16]) primitive2_max[31:16] <= v0x;
-					if(v0y<primitive2_min[15:0]) primitive2_min[15:0] <= v0y;
-					else if(v0y>primitive1_max[15:0]) primitive2_max[15:0] <= v0y;
-					
-				  if(primitive2_type == `RECTANGLE) begin
-					  primitive2_v3_buffer[31:16]<=v0x;
-					  primitive2_v3_buffer[15:0]<=v0y;
-				  end
-				  vertexCount <= 3'd0;
-				  primitive2_valid <= 1'b1;
-				end
-		   end
-		end
-		
-		rastCycle1 <= !stall & draw_reg;
-		rastCycle2 <= rastCycle1;
-		rastCycle3 <= rastCycle2;
-		rastCycle4 <= rastCycle3;
-		if(rastCycle4==1'b1) rastContinue <= 1'b1;
-		else if(rastContinue==1'b1 && rastDone==1'b0) rastContinue <= 1'b1;
-		else rastContinue <=1'b0;
+			end
+
+
 		
 		
 		
@@ -364,13 +368,25 @@ input iVGA_BLANK;
 		//if(counter>2) NewVertex <= 1'b0;
 		//else NewVertex <=1'b1;
 		
+		
+		rastCycle1 <= (~stall) & draw_reg;
+		//if(counter==1)rastCycle1<=1'b1;
+		//else 
+	  //if(counter==16'd6)rastCycle1<=1'b1;
+		//else rastCycle1<=1'b0;
+		rastCycle2 <= rastCycle1;
+		rastCycle3 <= rastCycle2;
+		rastCycle4 <= rastCycle3;
+		if(rastCycle4==1'b1) rastContinue <= 1'b1;
+		else if(rastContinue==1'b1 && rastDone==1'b0) rastContinue <= 1'b1;
+		else rastContinue <=1'b0;
 		  
 		//control signals for muxes. v0 is hardwired into v0 of the rasterizer
 		//A value of 1 selects the alternate vertex for drawing the other half of
 		// a restangle (v0,v2,v3). 0 selects (v0,v1,v2).
 		
-		if(primitive2_valid==1'b1 && primitive1_drawn==1'b1 && primitive1_valid==1'b1 && primitive2_drawn==1'b0) current_prim <= 1'b1;
-	   else current_prim <= 1'b0;
+		if(rastDone==1'b1 && primitive2_valid==1'b1 && primitive1_drawn==1'b1 && primitive2_drawn==1'b0) current_prim <= 1'b1;
+	  else if(rastDone==1'b1 && primitive2_drawn==1'b1 && primitive1_drawn==1'b1) current_prim <= 1'b0;
 
 		//rastDone will be asserted after initial 1/2 of prim1 is drawn.
 		if(rastDone==1'b1 && primitive1_drawn==1'b0 && primitive1_valid==1'b1) begin
@@ -396,7 +412,6 @@ input iVGA_BLANK;
 		  end	
 		end
 		end
-	//end
 	end
 	
 	assign v0_in = (current_prim==1'b0) ? primitive1_v0_buffer : primitive2_v0_buffer;
@@ -435,25 +450,16 @@ input iVGA_BLANK;
 						.out_pixel_x(pixelXout), .out_pixel_y(pixelYout),// x, y coordinates of any output pixels (only pixels inside triangle)
 						.out_pixel_depth(pixelDepth),							// depth (z-value) of any output pixels (only pixels inside triangle) - NOT YET IMPLEMENTED
 						.out_pixel_color(pixelColor));						// color of any output pixels (only pixels inside triangle));
-
-	//assign HEX0 = pixelYout[3:0];
-	//assign HEX1 = pixelYout[7:4];
-	//show y-component on hex displays
-	/*sevenSegNum sseg2(.NUM(v0_buffer[3:0]),.DISP(HEX0));
-	sevenSegNum sseg3(.NUM(v0_buffer[7:4]),.DISP(HEX1));
-	sevenSegNum sseg0(.NUM(v0_buffer[11:8]),.DISP(HEX2));
-	sevenSegNum sseg1(.NUM(v0_buffer[15:12]),.DISP(HEX3));*/
 	
-	
-	always@(posedge iCLK or posedge reset)
+/*always@(posedge iCLK or posedge reset)
 begin
 	if(reset)
 	begin
-		colInd		<=	blanked1 ? primitive2_min[15:0] : primitive1_min[15:0];
+		colInd		<=	0;
 	end
 	else if (!iVIDEO_ON && blankDone==1'b0)
 	begin
-		if( colInd < ( blanked1 ? primitive2_max[15:0] : primitive1_max[15:0] ) )
+		if( colInd < 639 )
 		colInd	<=	colInd+1;
 		else
 		colInd	<=	0;
@@ -464,22 +470,60 @@ always@(posedge iCLK or posedge reset)
 begin
 	if(reset)
 	begin
-		rowInd		<=	blanked1 ? primitive2_min[31:16] : primitive1_min[31:16];
+		rowInd		<=	0;
 	end
 	else if (!iVIDEO_ON && blankDone==1'b0)
 	begin
 		if(colInd==0)
 		begin
-			if( rowInd < ( blanked1 ? primitive2_max[31:16] : primitive1_max[31:16] ))
+			if( rowInd < 399 )
 			rowInd	<=	rowInd+1;
 			else
 			rowInd	<=	0;
 		end
 	end
+end*/
+
+always@(posedge iCLK)
+begin
+	if(resetBlank)
+	begin
+		colInd		<=	blanked1 ? primitive2_min[31:16] : primitive1_min[31:16];
+	end
+	else if (!iVIDEO_ON && blankDone==1'b0)
+	begin
+		if( colInd < ( blanked1 ? primitive2_max[31:16] : primitive1_max[31:16] ) )
+		colInd	<=	colInd+1'b1;
+		else
+		colInd	<=	blanked1 ? primitive2_min[31:16] : primitive1_min[31:16];
+	end
+end
+
+always@(posedge iCLK)
+begin
+	if(resetBlank)
+	begin
+		rowInd		<=	blanked1 ? primitive2_min[15:0] : primitive1_min[15:0];
+	end
+	else if (!iVIDEO_ON && blankDone==1'b0)
+	begin
+		if(colInd==( blanked1 ? primitive2_min[15:0] : primitive1_min[31:16]))
+		begin
+			if( rowInd < ( blanked1 ? primitive2_max[15:0] : primitive1_max[15:0] ))
+			rowInd	<=	rowInd+1'b1;
+			else
+			rowInd	<=	blanked1 ? primitive2_min[15:0] : primitive1_min[15:0];
+		end
+	end
 end
 
 
+
+
+
 endmodule
+
+
 
 
 
